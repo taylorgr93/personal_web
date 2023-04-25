18cc0faa243909b47ad9839c7c938dc1bf93bf29
ctgonzalez <taylor.gonzalez@knesysplus.com>
2023-04-25 17:28:05 -0600

    se cambia info de banner, about me y cv

--- src/data/aboutMeData.js
+++ src/data/aboutMeData.js
@@ -1,10 +1,16 @@
 export const aboutMeData = [
-	{
-		id: 1,
-		bio: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt totam dolorum, ducimus obcaecati, voluptas facilis molestias nobis ut quam natus similique inventore excepturi optio ipsa deleniti fugit illo. Unde, amet! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum illo necessitatibus perspiciatis! Aperiam perferendis labore temporibus, eos culpa corporis recusandae quas, fuga voluptatibus nesciunt odit libero tenetur neque consequatur ea.',
-	},
-	{
-		id: 2,
-		bio: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta?',
-	},
+  {
+    id: 1,
+    bio: `I am a software developer with over six years of experience in electronic design and development. Currently, I work as a software developer and have been with my current company for four years. For me, the work environment is the most important factor in a job.`,
+  },
+  {
+    id: 2,
+    bio: `At my current company, I have been responsible for developing IoT and Industry 4.0 solutions as a software developer. My work has involved working with embedded systems, particularly the Raspberry Pi with Linux-based operating systems such as Ubuntu, Manjaro, and Raspbian. Additionally, I have worked as a Full Stack developer, using the MERN stack which includes React, Node.js, and Express.js with MongoDB as the database. I have also utilized various programming tools like Typescript, Docker, JEST, Git, AWS, and web development languages such as HTML and CSS.`,
+  },
+  //   {
+  //     id: 3,
+  //     bio: `with React, Node JS Express.js, MongoDB, Linux OS,
+  // 	Docker, Raspberry PI, also I have experience programming language (C#,
+  // 	PYTHON), knowledge in HTML and CSS.`,
+  //   },
 ];
