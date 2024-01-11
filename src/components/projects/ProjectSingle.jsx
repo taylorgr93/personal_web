997d7d67ddf81fee5bb66de2cc2655afe290c4dc
Taylor Gonzalez <taylorgr@outlook.com>
2024-01-10 18:27:18 -0600

    projects

--- src/components/projects/ProjectSingle.jsx
+++ src/components/projects/ProjectSingle.jsx
@@ -13,25 +13,25 @@ const ProjectSingle = ({ title, category, image }) => {
         delay: 0.15,
       }}
     >
-      {/* <Link to="/projects/single-project" aria-label="Single Project">
-				<div className="rounded-xl shadow-lg hover:shadow-xl cursor-pointer mb-10 sm:mb-0 bg-secondary-light dark:bg-ternary-dark">
-					<div>
-						<img
-							src={image}
-							className="rounded-t-xl border-none"
-							alt="Single Project"
-						/>
-					</div>
-					<div className="text-center px-4 py-6">
-						<p className="font-general-medium text-lg md:text-xl text-ternary-dark dark:text-ternary-light mb-2">
-							{title}
-						</p>
-						<span className="text-lg text-ternary-dark dark:text-ternary-light">
-							{category}
-						</span>
-					</div>
-				</div>
-			</Link> */}
+      {/* <Link to="/projects/single-project" aria-label="Single Project"> */}
+      <div className="rounded-xl shadow-lg hover:shadow-xl cursor-pointer mb-10 sm:mb-0 bg-secondary-light dark:bg-ternary-dark">
+        <div>
+          <img
+            src={image}
+            className="rounded-t-xl border-none"
+            alt="Single Project"
+          />
+        </div>
+        <div className="text-center px-4 py-6">
+          <p className="font-general-medium text-lg md:text-xl text-ternary-dark dark:text-ternary-light mb-2">
+            {title}
+          </p>
+          <span className="text-lg text-ternary-dark dark:text-ternary-light">
+            {category}
+          </span>
+        </div>
+      </div>
+      {/* </Link> */}
     </motion.div>
   );
 };
