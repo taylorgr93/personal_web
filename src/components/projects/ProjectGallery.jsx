d6061475b8f30327898ab6663414bc381316c332
Taylor Gonzalez <taylorgr@outlook.com>
2024-09-25 17:05:42 -0600

    se agrega componente para ver imagenes en grande

--- src/components/projects/ProjectGallery.jsx
+++ src/components/projects/ProjectGallery.jsx
@@ -1,28 +1,54 @@
-import { useContext } from "react";
+import { useState, useContext } from "react";
 import SingleProjectContext from "../../context/SingleProjectContext";
 
 const ProjectGallery = () => {
   const { singleProjectData } = useContext(SingleProjectContext);
+  const [selectedImage, setSelectedImage] = useState(null);
 
   if (!singleProjectData || !singleProjectData.ProjectImages) {
     return <></>;
   }
 
+  const handleImageClick = (img) => {
+    setSelectedImage(img); // Almacena la imagen seleccionada
+  };
+
+  const closeModal = () => {
+    setSelectedImage(null); // Cierra el modal al limpiar la imagen seleccionada
+  };
+
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-3 sm:gap-10 mt-12">
-      {singleProjectData.ProjectImages.map((project) => {
-        return (
-          <div className="mb-10 sm:mb-0" key={project.id}>
+    <div>
+      <div className="grid grid-cols-1 sm:grid-cols-3 sm:gap-10 mt-12">
+        {singleProjectData.ProjectImages.map((project) => (
+          <div className="w-60 h-64 mb-10 sm:mb-0" key={project.id}>
             <img
               src={project.img}
-              // className="rounded-xl cursor-pointer shadow-lg sm:shadow-none"
-              className="rounded-xl shadow-lg sm:shadow-none"
+              className="rounded-xl cursor-pointer shadow-lg sm:shadow-none"
               alt={project.title}
-              key={project.id}
+              onClick={() => handleImageClick(project.img)} // Abrir el modal al hacer clic
             />
           </div>
-        );
-      })}
+        ))}
+      </div>
+
+      {selectedImage && (
+        <div className="fixed inset-0 bg-black bg-opacity-75 flex justify-center items-center z-50">
+          <div className="relative">
+            <button
+              className="absolute top-2 right-2 text-white text-2xl"
+              onClick={closeModal}
+            >
+              &times; {/* Botón para cerrar el modal */}
+            </button>
+            <img
+              src={selectedImage}
+              className="max-w-full max-h-full"
+              alt="Enlarged view"
+            />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
