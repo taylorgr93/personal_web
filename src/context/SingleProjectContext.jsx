4df10f5a2c18aba1c797f7eab4e749f1c88465fc
Taylor Gonzalez <taylorgr@outlook.com>
2024-09-24 23:59:59 -0600

    se agrega logica para que cada projecto cargue su propia informacion

--- src/context/SingleProjectContext.jsx
+++ src/context/SingleProjectContext.jsx
@@ -1,12 +1,32 @@
-import { useState, createContext } from "react";
-import { singleProjectData as singleProjectDataJson } from "../data/singleProjectData";
+import { useParams } from "react-router-dom";
+import { useState, createContext, useEffect } from "react";
+import { singleProjectDataArray } from "../data/singleProjectDataArray";
+import NotFound from "../components/NotFound";
 
 const SingleProjectContext = createContext();
 
 export const SingleProjectProvider = ({ children }) => {
-  const [singleProjectData, setSingleProjectData] = useState(
-    singleProjectDataJson
-  );
+  const { id } = useParams();
+  const [singleProjectData, setSingleProjectData] = useState();
+  const [projectExists, setProjectExists] = useState(true); // Estado para verificar si el proyecto existe
+
+  useEffect(() => {
+    // console.log("id:", id);
+    const projectId = Number(id) - 1; // Restar 1 para la indexación
+    const projectData = singleProjectDataArray[projectId];
+
+    if (projectData) {
+      setSingleProjectData(projectData);
+      setProjectExists(true); // El proyecto existe
+    } else {
+      setSingleProjectData(null);
+      setProjectExists(false); // El proyecto no existe
+    }
+  }, [id]);
+
+  if (!projectExists) {
+    return <NotFound />; // Retorna el componente NotFound si el proyecto no existe
+  }
 
   return (
     <SingleProjectContext.Provider
--- src/data/singleProjectDataArray.js
+++ src/data/singleProjectDataArray.js
@@ -1,19 +1,147 @@
+/*eslint-disable*/
+// Import images
+import Image1 from "../images/ui-project-1.jpg";
+import Image2 from "../images/web-project-2.jpg";
+import Image3 from "../images/mobile-project-2.jpg";
+import Image4 from "../images/mobile-project-1.jpg";
+import Image5 from "../images/web-project-1.jpg";
+import Image6 from "../images/ui-project-2.jpg";
+
+import ConboletoImg from "../images/web-project-6.png";
+
+// Import icons
+import {
+  FiFacebook,
+  FiInstagram,
+  FiTwitter,
+  FiLinkedin,
+  FiYoutube,
+  FiX,
+} from "react-icons/fi";
+
 export const singleProjectDataArray = [
   {
-    id: 1,
     ProjectHeader: {
-      title: "Project Management UI",
-      publishDate: "Jul 26, 2021",
-      tags: "UI / Frontend",
+      title: "Conboleto",
+      publishDate: "Jan 26, 2024",
+      tags: "Backend",
+    },
+    ProjectImages: [
+      {
+        id: 1,
+        title: "Conboleto UI",
+        img: ConboletoImg,
+      },
+      // {
+      //   id: 2,
+      //   title: "Kabul Project Management UI",
+      //   img: Image2,
+      // },
+      // {
+      //   id: 3,
+      //   title: "Kabul Project Management UI",
+      //   img: Image3,
+      // },
+    ],
+    ProjectInfo: {
+      ClientHeading: "About Client",
+      CompanyInfo: [
+        {
+          id: 1,
+          title: "Name",
+          details: "Company Ltd",
+        },
+        {
+          id: 2,
+          title: "Services",
+          details: "UI Design & Frontend Development",
+        },
+        {
+          id: 3,
+          title: "Website",
+          details: "https://company.com",
+        },
+        {
+          id: 4,
+          title: "Phone",
+          details: "555 8888 888",
+        },
+      ],
+      ObjectivesHeading: "Objective",
+      ObjectivesDetails:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, natus! Quibusdam enim quod in esse, mollitia molestias incidunt quas ipsa accusamus veniam.",
+      Technologies: [
+        {
+          title: "Tools & Technologies",
+          techs: [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "Vue.js",
+            "TailwindCSS",
+            "AdobeXD",
+          ],
+        },
+      ],
+      ProjectDetailsHeading: "Challenge",
+      ProjectDetails: [
+        {
+          id: 1,
+          details:
+            "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt totam dolorum, ducimus obcaecati, voluptas facilis molestias nobis ut quam natus similique inventore excepturi optio ipsa deleniti fugit illo. Unde, amet! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum illo necessitatibus perspiciatis! Aperiam perferendis labore temporibus, eos culpa corporis recusandae quas, fuga voluptatibus nesciunt odit libero tenetur neque consequatur ea.",
+        },
+        {
+          id: 2,
+          details:
+            "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta?",
+        },
+        {
+          id: 3,
+          details:
+            "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta?",
+        },
+        {
+          id: 4,
+          details:
+            "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt totam dolorum, ducimus obcaecati, voluptas facilis molestias nobis ut quam natus similique inventore excepturi optio ipsa deleniti fugit illo. Unde, amet! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum illo necessitatibus perspiciatis! Aperiam perferendis labore temporibus, eos culpa corporis recusandae quas, fuga voluptatibus nesciunt odit libero tenetur neque consequatur ea.",
+        },
+      ],
+      SocialSharingHeading: "Share This",
+      SocialSharing: [
+        {
+          id: 1,
+          name: "Twitter",
+          icon: <FiTwitter />,
+          url: "https://x.com/conboleto",
+        },
+        {
+          id: 2,
+          name: "Instagram",
+          icon: <FiInstagram />,
+          url: "https://www.instagram.com/conboleto",
+        },
+        {
+          id: 3,
+          name: "Facebook",
+          icon: <FiFacebook />,
+          url: "https://www.facebook.com/Conboleto/?locale=es_LA",
+        },
+      ],
+    },
+  },
+  {
+    ProjectHeader: {
+      title: "Checkton",
+      publishDate: "Aug 12, 2021",
+      tags: "Frontend",
     },
     // Other project data
   },
   {
-    id: 2,
     ProjectHeader: {
-      title: "Mobile UI Design",
+      title: "RTSP ABSA Industrial",
       publishDate: "Aug 12, 2021",
-      tags: "UI / Frontend",
+      tags: "Frontend",
     },
     // Other project data
   },
