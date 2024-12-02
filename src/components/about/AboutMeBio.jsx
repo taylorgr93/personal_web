e913437be23e6af499084eeac3e42b4d7f43d6aa
Taylor Gonzalez <taylorgr@outlook.com>
2024-12-01 21:52:20 -0600

    se actualiza cv y photo de pagina

--- src/components/about/AboutMeBio.jsx
+++ src/components/about/AboutMeBio.jsx
@@ -1,18 +1,19 @@
 import { useContext } from "react";
 import AboutMeContext from "../../context/AboutMeContext";
-import TaylorPhoto from "../../images/taylor_graduaction.jpg";
+// import TaylorPhoto from "../../images/taylor_graduaction.jpg";
+import TaylorPhoto from "../../images/taylor.png";
 
 const AboutMeBio = () => {
   const { aboutMe } = useContext(AboutMeContext);
 
   return (
-    <div className="block sm:flex sm:gap-10 mt-10 sm:mt-20">
-      <div className="w-full sm:w-1/4 mb-7 sm:mb-0">
-        <div className="flex-none w-52 relative">
+    <div className="block sm:flex sm:gap-10 mt-10 sm:mt-20 bg-blue-500">
+      <div className="w-full sm:w-1/4 mb-0 sm:mb-0 bg-red-400">
+        <div className="flex-none w-52 relative bg-green-400">
           <img
             src={TaylorPhoto}
             alt=""
-            className="absolute inset-0 w-full h-full object-cover rounded-lg"
+            className="absolute inset-0 w-full h-full object-fit rounded-lg"
             loading="lazy"
           />
         </div>
